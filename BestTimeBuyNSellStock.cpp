6035e1c381431891c067f8336fd59255c94d8140
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2021-10-16 13:23:26 -0400

    Update BestTimeBuyNSellStock.cpp

--- BestTimeBuyNSellStock.cpp
+++ BestTimeBuyNSellStock.cpp
@@ -14,12 +14,14 @@ public:
         int minn = prices[0],  maxx = prices[n-1], prev = 0, ans = 0;
         vector<int> pref(n, 0);
         
-        for(int i = 1; i < n; i++) {
+        for(int i = 1; i < n; i++) {                                //iterate forward thru prices to find max possible profit
             pref[i] = max(pref[i-1], prices[i] - minn); // calculating profit upto ith day from the start
             minn = min(minn, prices[i]);
         }
+        //pref[i] returns a vector where every index is the largest difference of values possible
+        //minn returns the lowest price found in the vector of prices
 
-        for(int i = n-2; i >= 0; i--) {
+        for(int i = n-2; i >= 0; i--) {                             //iterate backwards thru prices to 
             prev = max(prev, maxx - prices[i]); // calculating profit from ith day to last day
             ans = max(pref[i] + prev, ans); // calculating profit with 2 transactions with 1st before and 2nd after ith day
             maxx = max(maxx, prices[i]);   
