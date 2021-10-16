d844826236482e95f5d409166692ea1ef984d0cf
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2021-10-16 10:06:40 -0400

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+        unordered_map<int, int> om;
         for (int i = 0; i < nums.size(); ++i) {
-            if (mp.count(target - nums[i])) {
-                return {mp[target - nums[i]], i};
+            if (om.count(target - nums[i])) {
+                return {om[target - nums[i]], i};
             }
-            mp[nums[i]] = i;
+            om[nums[i]] = i;
         }
         
         return {};
