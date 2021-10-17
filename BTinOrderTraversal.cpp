5400bceb882396fab8025cbd7f49b2004ed080c5
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2021-10-17 12:29:14 -0400

    Create BTinOrderTraversal.cpp

---
+++ BTinOrderTraversal.cpp
@@ -0,0 +1,39 @@
+/*
+
+*/
+
+//recurive solution
+vector<int> v;
+vector<int> inorderTraversal(TreeNode* root) {
+    if(root == NULL) {
+      return v;
+    }
+    inorderTraversal(root->left);
+    v.push_back(root->val);
+    inorderTraversal(root->right);
+    return v;
+}
+
+//stack solution
+class Solution {
+public:
+    vector<int> v;
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;                         //making a stack of TreeNodes
+        while(root || !s.empty()) {                 
+            if(root != NULL) {
+                s.push(root);
+                root = root->left;
+            }
+            else {
+                root = s.top();                     //the root has no left children, go back
+                s.pop();                            //don't need this node's value to wait in stack
+                v.push_back(root->val);             //can simply add it to our output vector
+                root = root->right;                 //and check its right children
+            }
+        }
+        return v;
+    }
+};
+
+
