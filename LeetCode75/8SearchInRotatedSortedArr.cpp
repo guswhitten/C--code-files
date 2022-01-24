36fa19f2e90935b67b99c76ebf11b1c3043047e9
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-01-24 07:23:31 -0500

    Create 8SearchInRotatedSortedArr.cpp

---
+++ LeetCode75/8SearchInRotatedSortedArr.cpp
@@ -0,0 +1,49 @@
+/*
+LEETCODE 75 #8
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0, end=nums.size()-1, mid;
+        while (start<=end) {
+            mid=(start+end)/2;
+            
+            if (nums[mid]==target) {
+                return mid;
+            }
+            
+            if (nums[start]<=nums[mid]) {
+                
+                if ((target>=nums[start]) && (target <=nums[mid])) {
+                    end=mid-1;
+                }
+                else {
+                    start=mid+1;
+                }
+            }
+            else {
+                if(target<=nums[end] && target>=nums[mid])
+                    start=mid+1;
+                else
+                    end=mid-1;
+            }
+        }
+        return -1;
+        
+    }
+};
+
+/*
+Runtime: 3 ms
+Memory Usage: 11.1 MB
+*/
