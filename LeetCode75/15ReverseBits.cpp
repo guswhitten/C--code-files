6135546f78b4e01a0bf399c87f9f6bdcc66bf5e2
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-05-31 08:40:54 -0400

    Update 15ReverseBits.cpp

--- LeetCode75/15ReverseBits.cpp
+++ LeetCode75/15ReverseBits.cpp
@@ -13,20 +13,20 @@ Therefore, in Example 2 above, the input represents the signed integer -3 and th
 represents the signed integer -1073741825.
 
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        for (int i=0; i < 32; i++) {
+            ans <<= 1;
+            ans |= n&1;
+            n>>=1;
+        }
+        return ans;
+    }
+};
 
 /*
-
+Runtime: 0ms
+Memory:5.8MB
 */
