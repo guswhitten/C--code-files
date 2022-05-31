0b575496edf0784ae799d19ec63614beb95c576d
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-05-31 08:16:51 -0400

    Update 14MissingNumber.cpp

--- LeetCode75/14MissingNumber.cpp
+++ LeetCode75/14MissingNumber.cpp
@@ -1,1 +1,30 @@
+/*
+LEETCODE 75 #14
 
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+*/
+
+
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int missingNumber;
+        sort(nums.begin(),nums.end());
+        for (int i=0; i<nums.size();i++) {
+            if (nums[i] != i) {
+                missingNumber = i;
+                break;
+            }
+            
+        }
+        return missingNumber;
+    }
+};
+
+/*
+Runtime: 40ms
+Memory: 18MB
+*/
