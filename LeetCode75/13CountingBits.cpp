3e363a54d06767425ac1b294774acf5bf50527f5
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-05-31 08:06:58 -0400

    Update 13CountingBits.cpp

--- LeetCode75/13CountingBits.cpp
+++ LeetCode75/13CountingBits.cpp
@@ -1,1 +1,31 @@
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+
+
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for (int i = 0; i<=n; i++) {
+            int sum=0;
+            uint32_t m = i;
+            while (m>0) {
+                
+                if (m & 1 > 0) {
+                    sum++;
+                }
+                m=m>>1;
+            }
+            ans.push_back(sum);
+        }
+            
+        return ans;
+    }
+};
+
+/*
+
+
+*/
 
