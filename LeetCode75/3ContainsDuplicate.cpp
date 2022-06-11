db28f153e125f1da389a0bd3f7201c5b1f6c7ac6
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-06-11 11:23:24 -0400

    Update 3ContainsDuplicate.cpp

--- LeetCode75/3ContainsDuplicate.cpp
+++ LeetCode75/3ContainsDuplicate.cpp
@@ -8,13 +8,25 @@ Given an integer array nums, return true if any value appears at least twice in 
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        
+        //create map where 
+        //key={number in array 'nums'}
+        //value={frequency of that number}
         unordered_map<int,int> umap;
+        
+        //iterate thru array
         for (auto num : nums) {
+            
+            //increase frequency of current number by one
             umap[num]++;
+            
+            //if frequency ever exceeds 1 for any number, end program
             if (umap[num] > 1) {
                 return true;
             }
         }
+        
+        //no frequency ever exceeds 1 for any number. Unique array
         return false;
     }
 };
