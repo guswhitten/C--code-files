23a9449d57cc6d1a7fcbb491ed05ace85608df93
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-06-13 06:53:49 -0400

    Update 22LongestCommonSubsequence.cpp

--- LeetCode75/22LongestCommonSubsequence.cpp
+++ LeetCode75/22LongestCommonSubsequence.cpp
@@ -7,11 +7,41 @@ For example, "ace" is a subsequence of "abcde".
 A common subsequence of two strings is a subsequence that is common to both strings.
 */
 
-
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size(), n = text2.size();
+        
+        //create a padded dynamic programming matrix
+        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+        
+        //iterate thru matrix
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                
+                //compare every letter in text2 to i-th letter in text1
+                if (text1[i] == text2[j]) {
+                    
+                    //if they're equal, +1 from square diagonally top-left
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                }
+                
+                else {
+                    
+                    //otherwise, just carry the max longest sequence from square above or to the left
+                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j]);
+                }
+            }
+        }
+        
+        //return bottom right square of matrix
+        return dp[m][n];        
+    }
+};
 
 
 
 /*
-Runtime: 
-Memory: 
+Runtime: 60ms
+Memory: 13 MB
 */
