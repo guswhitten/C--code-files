081e33c78d1a6b98d1c7aab5d48b39f237e17a2d
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-06-15 08:13:54 -0400

    Create 26JumpGame.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

---
+++ LeetCode75/26JumpGame.cpp
@@ -0,0 +1,25 @@
+/*
+You are given an integer array nums. You are initially 
+positioned at the array's first index, and each element in the 
+array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+*/
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int farthest = 0;
+        for (int i = 0; i < n; i++) {
+            if (farthest < i) return false;
+            farthest = max(i+nums[i], farthest);
+        }
+        return true;
+    }
+};
+
+/*
+Runtime: 71 ms
+Memory: 48.5 MB
+*/
