7f9d4f052cc713ed33abc29947962be7e42a0a8c
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-06-16 11:11:42 -0400

    Update 28CourseSchedule.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

--- LeetCode75/28CourseSchedule.cpp
+++ LeetCode75/28CourseSchedule.cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& p) {
         
-        if(p.size()==0) return true; //all courses independent
+        if(p.empty()) return true; //all courses independent
         
         //indegree counts the number of edges approaching a particular node
         vector<int> indegree(n, 0); 
@@ -20,12 +20,10 @@ public:
         vector<vector<int>> graph(n); 
         
         //iterate thru entire matrix of prerequisite courses
-        for(auto &x : p){
-            
+        for(auto x : p){
             //x[0] is the course with a prerequsite. x[1] is the prerequisite.
             //graph matrix flips them all.
             graph[x[1]].push_back(x[0]);
-          
             //index i of indegree is the course #. value at indegree[i] is how many inputs that node has
             indegree[x[0]]++;
         }
@@ -35,29 +33,20 @@ public:
         for(int i=0; i<n; i++){
             if(indegree[i]==0) q.push(i), indegree[i]--;
         }
-        //cycle exists if there is no node with indegree 0
-        if(q.size()==0) return false; 
-        
-        int count = 0; //No. of nodes you're able to reach till the end
-      
+        if(q.empty()) return false;  //cycle exists if there is no node with indegree 0
+        int taken = 0; //No. of nodes you're able to reach till the end
         while(!q.empty()){
-            int size = q.size();
-            
-            //iterate thru queue (filled with the courses, p[0], that have 0 indegrees)
-            for(int i=0; i<size; i++){
-                int curr = q.front(); q.pop();
-                count++;
-                for(auto child : graph[curr]){
-                    indegree[child]--;  //decrement the indegree of the nodes until you've reached all nodes
-                    if(indegree[child]==0) q.push(child);
-                }
-            }   
+            int curr = q.front(); 
+            q.pop();
+            taken++;
+            for(auto child : graph[curr]){
+                indegree[child]--;  //decrement the indegree of the nodes until you've reached all nodes
+                if(indegree[child]==0) q.push(child);
+            }
         }
-        if(count==n) return true;
-        return false;
+        return taken == n;
     }
 };
-
 
 
 /*
