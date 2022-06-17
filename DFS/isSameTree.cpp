b7f132263bf6af827208af9a2e3e5f018b767663
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-06-17 09:32:15 -0400

    Update isSameTree.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

--- DFS/isSameTree.cpp
+++ DFS/isSameTree.cpp
@@ -20,7 +20,10 @@ they are structurally identical, and the nodes have the same value.
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p==nullptr && q==nullptr) return true;
+        //if both have reached their end pointers at the same time, they are equal
+        if (p==nullptr && q==nullptr) return true;      
+        
+        //if both have NOT reached their ends at the same time, they are unequal
         if (p==nullptr || q==nullptr) return false;
         if (p->val != q->val) return false;
         return isSameTree(p->left,q->left) && isSameTree(p->right, q->right);
