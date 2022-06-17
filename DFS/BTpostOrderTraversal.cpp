9b9d0ec848a17776b9a472c018f0f75a541b0461
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-06-17 08:52:35 -0400

    Update BTpostOrderTraversal.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

--- DFS/BTpostOrderTraversal.cpp
+++ DFS/BTpostOrderTraversal.cpp
@@ -1,3 +1,8 @@
+/*
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+*/
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,6 +14,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//recursive implementation
+
+class Solution {
+public:
+    vector<int> v;
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (!root) return v;
+        postorderTraversal(root -> left);
+        postorderTraversal(root -> right);
+        v.push_back(root -> val);
+        return v;
+    }
+};
+
+
+//stack implementation
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode *root) {
