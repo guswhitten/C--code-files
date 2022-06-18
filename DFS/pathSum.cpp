f47c9185329249aed510f2498e00f12186f920f9
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-06-18 14:06:40 -0400

    Update pathSum.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

--- DFS/pathSum.cpp
+++ DFS/pathSum.cpp
@@ -1,5 +1,45 @@
 /*
-Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+Given the root of a binary tree and an integer targetSum, 
+return true if the tree has a root-to-leaf path such that 
+adding up all the values along the path equals targetSum.
 
 A leaf is a node with no children.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == NULL) return false;
+        int sum = 0;
+        return dfs(root, targetSum, sum);
+    }
+    
+    bool dfs(TreeNode* root, int targetSum, int sum) {
+        if (root == nullptr) return false;
+        sum += root -> val;
+        if (sum == targetSum) {
+            if ((root -> left == nullptr) && (root -> right == nullptr))
+                return true;
+        }
+        if (dfs(root -> left, targetSum, sum)) return true;
+        if (dfs(root -> right, targetSum, sum)) return true;
+        sum -= root -> val;
+        return false;
+    }
+};
+
+/*
+Runtime: 10 ms
+Memory: 21.2 MB
 */
