b1c3534d026b19b5a7a5e7ba609184214c5f3eaa
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-06-22 13:53:17 -0400

    Update 40lowestCommonAncestorBST.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

--- LeetCode75/40lowestCommonAncestorBST.cpp
+++ LeetCode75/40lowestCommonAncestorBST.cpp
@@ -7,8 +7,46 @@ common ancestor is defined between two nodes p and q as the lowest node in T tha
 q as descendants (where we allow a node to be a descendant of itself).”
 */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* ans;
+    bool flag = true;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        dfs(root, p, q, flag);
+        return ans;
+    }
+    
+    void dfs(TreeNode* root, TreeNode* p, TreeNode* q, bool flag) {
+        if (!root) return;
+        if (!flag) return;
+        if (p->val <= root->val && q->val >= root-> val) {
+            ans=root;
+            flag=false;
+            return;
+        }
+        else if (p->val >= root->val && q->val <= root-> val) {
+            ans=root;
+            flag=false;
+            return;
+        }        
+        dfs(root->left, p, q, flag);
+        dfs(root->right, p, q, flag);
+        return;
+        
+    }
+};
 
 /*
-Runtime: 
-Memory: 
+Runtime: 42 ms
+Memory: 23.2 MB
 */
