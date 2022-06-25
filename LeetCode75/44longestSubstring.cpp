0a00826098ca60153ba85316dcee1880e90a35f6
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-06-25 09:51:46 -0400

    Update 44longestSubstring.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

--- LeetCode75/44longestSubstring.cpp
+++ LeetCode75/44longestSubstring.cpp
@@ -5,13 +5,15 @@ Given a string s, find the length of the longest substring without repeating cha
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> dict(256, -1);
+        vector<int> dict(256, -1);           //vector of size 256 to account for entire ASCII table
         int maxLen = 0, start = -1;
         for (int i = 0; i < s.length(); i++) {
-            if (dict[s[i]] > start)
-                start = dict[s[i]];
-            dict[s[i]] = i;
-            maxLen = max(maxLen, i - start);
+            if (dict[s[i]] > start)         //TRUE if this character has been seen before
+                //start keeps track of the index (right before the one) that begins our LOCALLY LONGEST STRING
+                start = dict[s[i]];         //update start to signal a new string
+            dict[s[i]] = i;                 //value at each index of dict records LAST INDEX AT WHICH the current letter was last seen
+            //i - start     -->     checks the length of the current 
+            maxLen = max(maxLen, i - start); 
         }
         return maxLen;
     }
