5b80054ed662c17dac9360b3b2e91d01e526f42d
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-06-25 09:14:45 -0400

    Create 44longestSubstring.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

---
+++ LeetCode75/44longestSubstring.cpp
@@ -0,0 +1,23 @@
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
+
+/*
+Runtime: 12 ms
+Memory: 8.3 MB
+*/
