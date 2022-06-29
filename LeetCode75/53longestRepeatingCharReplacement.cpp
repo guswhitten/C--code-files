efa458da0b7ceb45e9b6603928703fd5b1a40a71
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-06-29 13:45:38 -0400

    Update 53longestRepeatingCharReplacement.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

--- LeetCode75/53longestRepeatingCharReplacement.cpp
+++ LeetCode75/53longestRepeatingCharReplacement.cpp
@@ -11,18 +11,21 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         //use sliding window approach
-        unordered_map<char, int> um;
-        int i=0, j=0, maxi=0, n=s.size();
-        int ans=-1;
-        while (j < n) {
-            um[s[j]]++;
-            maxi=max(maxi, um[s[j]]);
-            if ((j-i+1) - maxi > k) {
-                um[s[i]]--;
-                i++;
+        unordered_map<char, int> um;        //stores frequency of letter
+        int windowStart=0;                  //keeps track of window's starting index
+        int windowEnd=0;                    //keeps track of window's ending index
+        int maxi=0;                         //keeps track of most frequent letter in string
+        int n=s.size();
+        int ans=-1;                         //return -1 if string is empty
+        while (windowEnd < n) {             //iterate thru string
+            um[s[windowEnd]]++;             //map freq of each letter
+            maxi=max(maxi, um[s[windowEnd]]);       
+            if ((windowEnd-windowStart+1) - maxi > k) {       //CURRENT WINDOW
+                um[s[windowStart]]--;                 //remove first letter from map
+                windowStart++;                        //
             }
-            ans=max(ans, (j-i+1));
-            j++;
+            ans=max(ans, (windowEnd-windowStart+1));
+            windowEnd++;
         }
         return ans;
     }
