4133f93e17be5d82b69bad83a5ce57745164a7c2
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-07-07 08:28:54 -0400

    Update avgOfLevelsInBT.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

--- BFS/avgOfLevelsInBT.cpp
+++ BFS/avgOfLevelsInBT.cpp
@@ -4,11 +4,48 @@ each level in the form of an array.
 Answers within 10-5 of the actual answer will be accepted.
 */
 
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> ans;
+        queue<TreeNode*> q;
+        if (!root) return {{}};
+        q.push(root);
+        double avg, total, sum;
+        
+        while (!q.empty()) {
+            sum=0;
+            int n = q.size();
+            total = n;                  //save # of nodes in row to compute avg
+            while (n>0) {               //build queue for next row
+                TreeNode* top = q.front();
+                sum += top->val;        //sum all nodes in the row
+                q.pop();
+                if (top->left) q.push(top->left);     //build queue for next row
+                if (top->right) q.push(top->right);   //build queue for next row
+                n--;
+            }
+            avg=sum/total;
+            ans.push_back(avg);
+        }
+        return ans;
+    }
+};
 
 
 
 /*
-Runtime: 
-Memory: 
+Runtime: 15 ms
+Memory: 22.4 MB
 */
