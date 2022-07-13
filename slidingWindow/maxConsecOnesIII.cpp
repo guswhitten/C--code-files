38e34c8c0995b64b6df1bb22c67ee99fdd9edc85
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-07-13 10:17:11 -0400

    Update maxConsecOnesIII.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

--- slidingWindow/maxConsecOnesIII.cpp
+++ slidingWindow/maxConsecOnesIII.cpp
@@ -6,11 +6,17 @@ consecutive 1's in the array if you can flip at most k 0's.
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int start=0, countZeros=0, ans=INT_MIN;
+        //start-> starting index of window
+        //end->ending index of window
+        //strategy:
+        //  extend window until there are k 0's inside (our allowance)
+        //  then if new index is a 0, contract starting index until you've past the first 0
+        
+        int start = 0, ans=0, numZeros=0;
         for (int end = 0; end < nums.size(); end++) {
-            if (nums[end] == 0) countZeros++;
-            while (countZeros > k) {
-                if (nums[start]==0) countZeros--;
+            if (nums[end] == 0) numZeros++;             //another zero in window
+            while (numZeros > k) {
+                if (nums[start]==0) numZeros--;
                 start++;
             }
             ans=max(ans, end-start+1);
