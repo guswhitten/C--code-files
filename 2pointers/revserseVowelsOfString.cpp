a11fe489e912977ece08826b099e7370006e191d
Whitten Oswald <90280763+guswhitten@users.noreply.github.com>
2022-07-15 10:31:39 -0400

    Create revserseVowelsOfString.cpp
    Signed-off-by: Whitten Oswald <90280763+guswhitten@users.noreply.github.com>

---
+++ 2pointers/revserseVowelsOfString.cpp
@@ -0,0 +1,40 @@
+/*
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both cases.
+*/
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string temp ="";
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+                temp+=s[i];
+            }
+            else if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+                temp+=s[i];
+            }
+        }
+        int j=0;
+        reverse(temp.begin(), temp.end());
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+                s[i]=temp[j];
+                j++;
+            }
+            else if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+                s[i]=temp[j];
+                j++;
+            }
+        }
+        return s;
+    }
+};
+
+
+
+/*
+Runtime: 12ms
+Memory: 7.8 MB
+*/
